60ebab721b6d8115d9c9b756e71a2e0e835f8af8
Ivan Molto <ivanmolto@gmail.com>
2018-07-27 20:23:05 +0200

    Ballot Basic - Solidity smart contract

---
+++ BallotBasic.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.24;
+
+contract BallotBasic {
+
+    struct Voter {
+        uint weight;
+        bool voted;
+        uint8 vote;
+    }
+
+    struct Proposal {
+        uint voteCount;
+    }
+
+    address chairperson;
+    mapping(address => Voter) voters;
+    Proposal[] proposals;
+
+
+    constructor(uint8 _numProposals) public {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 2;
+        proposals.length = _numProposals;
+    }
+
+    function register(address toVoter) public {
+        if (msg.sender != chairperson || voters[toVoter].voted) return;
+        voters[toVoter].weight = 1;
+        voters[toVoter].voted = false;
+    }
+
+    function vote(uint8 toProposal) public {
+        Voter storage sender = voters[msg.sender];
+        if (sender.voted || toProposal >= proposals.length) return;
+        sender.voted = true;
+        sender.vote = toProposal;
+        proposals[toProposal].voteCount += sender.weight;
+    }
+
+    function winningProposal() public constant returns (uint8 _winningProposal) {
+        uint256 winningVoteCount = 0;
+        for (uint8 prop = 0; prop < proposals.length; prop++)
+            if (proposals[prop].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[prop].voteCount;
+                _winningProposal = prop;
+        }
+    }
+}
